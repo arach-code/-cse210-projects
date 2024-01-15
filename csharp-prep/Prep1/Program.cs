415cbb662a243aa438054b68f4694e9314998c69
arach-code <aarach72@gmail.com>
2024-01-15 21:26:57 +0300

    added console.readline

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string firstname = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"your name is {lastname}, {firstname}, {lastname}");
+
+        Console.ReadLine();
     }
 }
